ee8cc2a36813d7fc785d12fb89668a9dd42957d7
Denya56 <56lkj546@gmail.com>
2022-06-22 19:11:19 +0200

    xd

--- Project/DTOs/UserAuth/UserDTO.cs
+++ Project/DTOs/UserAuth/UserDTO.cs
@@ -1,13 +1,16 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Project.DTOs.UserAuth
 {
     public class UserDTO
     {
+        [Required]
         [StringLength(15, ErrorMessage = "Login is too long. 15 characters max")]
         [MinLength(3, ErrorMessage = "Login is too short. 3 characters min")]
-        [RegularExpression(@"^[a-zA-Z0-9_-]+$", ErrorMessage = "Login can only contain lower or uppercase letters, numbers and/or _ -")]
+        [RegularExpression(@"^[a-zA-Z0-9_-]{3,}$", ErrorMessage = "Login can only contain lower or uppercase letters, numbers and/or _ -")]
         public string Login { get; set; }
+        [Required]
         //[RegularExpression(@"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$", ErrorMessage = "Minimum eight characters, at least one upper case English letter, one lower case English letter, one number and one special character")]
         public string Password { get; set; }
         //public string Email { get; set; }
